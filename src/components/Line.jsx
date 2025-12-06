9d9bf5dacaad8afdadefb5d294224f0b8bd2888d
shanewbarnes <shanewbarnes@outlook.com>
2025-12-05 20:28:54 -0500

    Implements Line component

---
+++ src/components/Line.jsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+import "./Line.css";
+
+function Line({ position }) {
+  const color = "red";
+  const strokeWidth = 8;
+
+  return (
+    <svg className="line-svg">
+        <line
+          x1={position.x1}
+          x2={position.x2}
+          y1={position.y1}
+          y2={position.y2}
+          stroke={color}
+          strokeWidth={strokeWidth}
+        />
+    </svg>
+  )
+}
+
+export default Line;
